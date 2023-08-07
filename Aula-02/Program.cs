057ac2f25ffcccd57090b98d30698cb8c50446e6
Mayki Douglas <mayki.douglas1@gmail.com>
2023-08-06 22:42:07 -0300

    Criando um projeto no padrão C# .NET

---
+++ Aula-02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program{
+    static void Main(string [] args){
+        Console.WriteLine("buceta de C# do caralho quero um curso atualizado buceta"); //editando o hello world
+        if(args.GetLength(0)>0){
+         Console.Write(args.GetValue(0)); //criado uma estrutura condicional para usar os argumentos de entrada na hora de executar o programa
+        }
+    }
+}
+
+// arquivo de C# criado em base no dotnet
