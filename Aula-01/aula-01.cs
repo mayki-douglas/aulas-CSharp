12be20f245ab1b416d2e7924b06e14f4664e76c7
Mayki Douglas <mayki.douglas1@gmail.com>
2023-08-06 22:41:43 -0300

    Inserindo o Hello World no C# Puro

---
+++ Aula-01/aula-01.cs
@@ -0,0 +1,7 @@
+using System; //forma de buscar uma biblioteca.
+
+class Master{ //criação de classe
+    static void Main(){ //função Main para executar o arquivo
+        Console.Write("hello world");
+    }
+}
